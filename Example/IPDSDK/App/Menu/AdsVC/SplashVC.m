d348092f973832bf856a4092608d6a19cd94cdc9
Mountain King <1054293440@qq.com>
2022-08-17 17:43:43 +0800

    开屏。 上一条写错了，不是视频内容，是视频流

--- Example/IPDSDK/App/Menu/AdsVC/SplashVC.m
+++ Example/IPDSDK/App/Menu/AdsVC/SplashVC.m
@@ -8,8 +8,9 @@
 
 #import "SplashVC.h"
 
-@interface SplashVC ()
-
+@interface SplashVC ()<IPDSplashAdDelegate>
+@property(nonatomic,strong) IPDSplashAd *splashAd;
+@property(nonatomic,strong) UIView *bottomView;
 @end
 
 @implementation SplashVC
@@ -21,25 +22,100 @@
 
 - (void)viewWillAppear:(BOOL)animated{
     [super viewWillAppear:animated];
-//    @"J8595471688",@"J9666281550"
+//    @"zjad_G9040714184494018",@"J8648995207",@"J5621495755",@"zjad_T887381439",@"c887417368"
     if (self.isFirstLoad) {
-        [self loadAd:@"J8595471688"];
+        [self loadAd:@"J8648995207"];
         self.isFirstLoad = NO;
     }
 }
 
 -(void) loadAd:(NSString*) adId{
+    self.splashAd = [[IPDSplashAd alloc] initWithPlacementId:adId];
+    self.splashAd.delegate = self;
+    self.splashAd.fetchDelay = 5.0;
+    self.splashAd.rootViewController = self;
+    self.splashAd.customBottomView = self.bottomView;
+    [self.splashAd loadAd];
+}
+
+#pragma mark =============== IPDSplashAdDelegate ===============
+/**
+ *  开屏广告素材加载成功
+ */
+-(void)ipd_splashAdDidLoad:(IPDSplashAd *)splashAd{
+    [self.splashAd showAdInWindow:[UIApplication sharedApplication].keyWindow];
+}
+
+/**
+ *  开屏广告成功展示
+ */
+-(void)ipd_splashAdSuccessPresentScreen:(IPDSplashAd *)splashAd{
 
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+/**
+ *  开屏广告点击回调
+ */
+- (void)ipd_splashAdClicked:(IPDSplashAd *)splashAd{
+    
 }
-*/
+
+/**
+ *  开屏广告关闭回调
+ */
+- (void)ipd_splashAdClosed:(IPDSplashAd *)splashAd{
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
+
+/**
+ *  开屏广告即将关闭回调
+ */
+- (void)ipd_splashAdWillClose:(IPDSplashAd *)splashAd{
+    
+}
+
+
+/**
+ *  应用进入后台时回调
+ *  详解: 当点击下载应用时会调用系统程序打开，应用切换到后台
+ */
+- (void)ipd_splashAdApplicationWillEnterBackground:(IPDSplashAd *)splashAd{
+}
+
+/**
+ * 开屏广告倒记时结束
+ */
+- (void)ipd_splashAdCountdownEnd:(IPDSplashAd*)splashAd{
+    
+}
+
+/**
+ *  开屏广告错误
+ */
+- (void)ipd_splashAdError:(IPDSplashAd *)splashAd withError:(NSError *)error{
+    NSLog(@"error:%@",error);
+}
+
+- (void)ipd_splashAdAdapterDidRewardEffective:(IPDSplashAd *)splashAd withInfo:(NSDictionary *)info{
+    NSLog(@"奖励触发，回调信息:%@",info);
+}
+
+#pragma mark =============== lazyload ===============
+-(UIView *)bottomView{
+    if (!_bottomView) {
+        _bottomView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, SCREEN_W, 100)];
+        _bottomView.backgroundColor = [UIColor whiteColor];
+        
+        UILabel *titleLabel = [[UILabel alloc]init];
+        titleLabel.textColor = [UIColor redColor];
+        titleLabel.font = [UIFont boldSystemFontOfSize:24];
+        titleLabel.text = @"LOGO";
+        [_bottomView addSubview:titleLabel];
+    }
+    return _bottomView;
+}
+
+
 
 @end
